c00625aafa1bf3721b3a811365ae53c1e4c5ffa8
potaore <potaore@gmail.com>
2020-01-01 11:24:17 +0900

    v1.3.2

--- solve_sudoku.js
+++ solve_sudoku.js
@@ -2,7 +2,7 @@ var exports = exports;
 if (!exports) exports = {};
 var solver = exports;
 (function () {
-    var version = "1.3.1";
+    var version = "1.3.2";
     var CELL_LENGTH = 9;
 
     var hashMemo = [], hashMemoLog2 = [], hashLengthMemo = [];
