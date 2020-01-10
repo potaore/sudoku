de87e44de358d0520a99617c98ef1867cb78ee89
potaore <potaore@gmail.com>
2020-01-10 19:43:45 +0900

    v1.5.0

--- solve_sudoku.js
+++ solve_sudoku.js
@@ -2,7 +2,7 @@ var exports = exports;
 if (!exports) exports = {};
 var solver = exports;
 (function () {
-    var version = "1.4.0";
+    var version = "1.5.0";
     var pf;
     (function () {
         pf = {
